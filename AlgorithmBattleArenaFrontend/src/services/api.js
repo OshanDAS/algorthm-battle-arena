850a3975c3e7656384f33b9fc4675bc5873b806e
ChanuRasmika <chanurashmika.edu@gmail.com>
2025-09-23 19:13:25 +0530

    fixing issues in CI/CD

--- AlgorithmBattleArenaFrontend/src/services/api.js
+++ AlgorithmBattleArenaFrontend/src/services/api.js
@@ -15,7 +15,7 @@ class ApiService {
 
   setAuthToken(token) {
     if (token) {
-      this.client.defaults.headers.common['Authorization'] = Bearer ${token};
+      this.client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     } else {
       delete this.client.defaults.headers.common['Authorization'];
     }
@@ -31,76 +31,23 @@ class ApiService {
 
   lobbies = {
     getAll: () => this.client.get('/api/Lobbies'),
-    getById: (id) => this.client.get(/api/Lobbies/${id}),
+    getById: (id) => this.client.get(`/api/Lobbies/${id}`),
     create: (data) => this.client.post('/api/Lobbies', data),
-    join: (id) => this.client.post(/api/Lobbies/${id}/join),
-    leave: (id) => this.client.post(/api/Lobbies/${id}/leave),
-    delete: (id) => this.client.delete(/api/Lobbies/${id})
+    join: (id) => this.client.post(`/api/Lobbies/${id}/join`),
+    leave: (id) => this.client.post(`/api/Lobbies/${id}/leave`),
+    delete: (id) => this.client.delete(`/api/Lobbies/${id}`)
   };
 
   matches = {
-    start: (lobbyId, data) => this.client.post(/api/Matches/${lobbyId}/start, data)
+    start: (lobbyId, data) => this.client.post(`/api/Matches/${lobbyId}/start`, data)
   };
 
   problems = {
     getAll: (filters) => this.client.get('/api/Problems', { params: filters }),
-    getById: (id) => this.client.get(/api/Problems/${id}),
+    getById: (id) => this.client.get(`/api/Problems/${id}`),
     create: (data) => this.client.post('/api/Problems', data),
-    update: (id, data) => this.client.put(/api/Problems/${id}, data),
-    delete: (id) => this.client.delete(/api/Problems/${id})
-  };
-}
-
-export default new ApiService();import axios from 'axios';
-
-// Automatically detect environment and use appropriate API URL
-const BASE_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:5000'  // Local development
-  : 'https://algorithmbattlearena-dwdmb7a6c0a7hqdc.southindia-01.azurewebsites.net'; // Production
-
-class ApiService {
-  constructor() {
-    this.client = axios.create({
-      baseURL: BASE_URL,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
-
-  setAuthToken(token) {
-    if (token) {
-      this.client.defaults.headers.common['Authorization'] = Bearer ${token};
-    } else {
-      delete this.client.defaults.headers.common['Authorization'];
-    }
-  }
-
-  auth = {
-    login: (email, password) => this.client.post('/api/Auth/login', { email, password }),
-    registerStudent: (data) => this.client.post('/api/Auth/register/student', data),
-    registerTeacher: (data) => this.client.post('/api/Auth/register/teacher', data),
-    refreshToken: () => this.client.get('/api/Auth/refresh/token'),
-    getProfile: () => this.client.get('/api/Auth/profile')
-  };
-
-  lobbies = {
-    getAll: () => this.client.get('/api/Lobbies'),
-    getById: (id) => this.client.get(/api/Lobbies/${id}),
-    create: (data) => this.client.post('/api/Lobbies', data),
-    join: (id) => this.client.post(/api/Lobbies/${id}/join),
-    leave: (id) => this.client.post(/api/Lobbies/${id}/leave),
-    delete: (id) => this.client.delete(/api/Lobbies/${id})
-  };
-
-  matches = {
-    start: (lobbyId, data) => this.client.post(/api/Matches/${lobbyId}/start, data)
-  };
-
-  problems = {
-    getAll: (filters) => this.client.get('/api/Problems', { params: filters }),
-    getById: (id) => this.client.get(/api/Problems/${id}),
-    create: (data) => this.client.post('/api/Problems', data),
-    update: (id, data) => this.client.put(/api/Problems/${id}, data),
-    delete: (id) => this.client.delete(/api/Problems/${id})
+    update: (id, data) => this.client.put(`/api/Problems/${id}`, data),
+    delete: (id) => this.client.delete(`/api/Problems/${id}`)
   };
 }
 
